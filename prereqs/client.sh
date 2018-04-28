0ef388967b137c2ee5545cfacf0fe394df7dcfda
Ewan <ewangitcommit@outlook.com>
2018-04-28 20:16:01 +0100

    add terraform to prereqs

--- prereqs/client.sh
+++ prereqs/client.sh
@@ -2,3 +2,7 @@
 
 curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
 sudo apt-get install git-lfs
+
+pushd bin
+curl -k https://releases.hashicorp.com/terraform/0.11.7/terraform_0.11.7_linux_amd64.zip | bsdtar -xf- -C .
+chmod 700 ./terraform
